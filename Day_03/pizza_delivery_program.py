ccc1d4ecbb788fa19f4a47fda0acec557eb6a61c
BoitumeloRamalepe <boitumeloramalape31@gmail.com>
2026-01-03 01:42:14 +0200

    Refactor pizza billing logic to check if  size is valid  before adding extras

--- Day_03/pizza_delivery_program.py
+++ Day_03/pizza_delivery_program.py
@@ -15,59 +15,24 @@ extra_cheese = input("Do you want extra cheese? Y or N: ")
 #       > Add extra cheese for any size pizza (Y or N): +$1
 
 
-# MY SOLUTION BELOW 
-bill =0 
-if  size == "S":
-    bill += 15
-    
-    if pepperoni == "Y":
-        bill += 2
+bill = 0
 
+if size == "S":
+    bill += 15
 elif size == "M":
     bill += 20
-
-    if pepperoni == "Y":
-        bill += 3
-
 elif size == "L":
     bill += 25
-
-    if pepperoni == "Y":
-        bill += 3
-
 else:
     print("You have chosen an invalid size.")
 
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
 if extra_cheese == "Y":
-    bill +=1
+    bill += 1
 
-print (f"Your final bill is : ${bill}")
-
-
-# Provided Solution
-
-# todo: work out how much they need to pay based on their size choice.
-
-# bill = 0
-
-# if size == "S":
-#     bill += 15
-# elif size == "M":
-#     bill += 20
-# elif size == "L":
-#     bill += 25
-# else:
-#     print("You have chosen an invalid size.")
-
-# # todo: work out how much to add to their bill based on their pepperoni choice.
-# if pepperoni == "Y":
-#     if size == "S":
-#         bill += 2
-#     else:
-#         bill += 3
-
-# # todo: work out their final amount based on whether if they want extra cheese.
-# if extra_cheese == "Y":
-#     bill += 1
-
-# print(f"Your final bill is: ${bill}.")
+print(f"Your final bill is: ${bill}.")
