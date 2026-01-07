2c99a22186e63f9c3e37b8962344f5c0349536d7
BoitumeloRamalepe <boitumeloramalape31@gmail.com>
2026-01-07 12:41:37 +0200

    Improved the Rock-Paper-Scissors game by storing the ASCII art in a list to reduce repetition and make the code easier to manage

--- Day_04/rock_paper_scissors_project.py
+++ Day_04/rock_paper_scissors_project.py
@@ -27,44 +27,39 @@ scissors ='''
 ---.__(___)
 '''
 
+game_images =[rock,paper,scissors]
 
 person = int(input("What do you chooose?" \
             "Type 0 for rock, 1 for paper or 2 for scissors: \n"))
 
+if 0<= person<=2:
+    print(game_images[person])
 
-computer = random.randint(0,2)
-#print(f"Computer chose: {computer}")
+    computer = random.randint(0,2)
+    print(f"Computer chose:{computer} \n {game_images[computer]}")
 
-if person == 0 and computer == 1 :
+    if person == computer:
+        print("It's a Draw")
 
-    print(rock)
-    print(f"Computer chose: {computer}\n {paper}")
-    print("You lose!")
+    elif person == 0 and computer == 1 :
+        print("You lose!")
 
-elif person == 0 and computer == 2 :
-    print(rock)
-    print(f"Computer chose: {computer}\n {scissors}")
-    print("You win!")
+    elif person == 0 and computer == 2 :
+        print("You win!")
 
-elif person == 1 and  computer == 0 :
-    print(paper)
-    print(f"Computer chose: {computer}\n {rock}")
-    print("You win!")
+    elif person == 1 and  computer == 0 :
+        print("You win!")
 
-elif person == 1 and computer == 2 :
-    print(paper)
-    print(f"Computer chose: {computer}\n {scissors}")
-    print("You lose!")
+    elif person == 2 and computer == 1 :
+        print("You win!")
+
+    elif person == 1 and computer == 2 :
+        print("You lose!")
     
-elif person == 2 and computer == 0 :
-    print(scissors)
-    print(f"Computer chose: {computer}\n {rock}")
-    print("You lose!")
+    elif person == 2 and computer == 0 :
+        print("You lose!")
     
-elif person == 2 and computer == 1 :
-    print(scissors)
-    print(f"Computer chose: {computer}\n {paper}")
-    print("You win!")
+
 
 else:
-    print("Draw")
+   print("You typed an invalid number. You lose!")
